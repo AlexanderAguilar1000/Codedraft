03b1eafa04424ad66e91910d7cd4bc263edf759d
Alexander <cresistem99@gmail.com>
2026-07-13 20:23:07 -0500

    primer commit

---
+++ codedraft/src/main/java/com/proyecto/codedraft/CodedraftApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.codedraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodedraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodedraftApplication.class, args);
+	}
+
+}
---
+++ codedraft/src/test/java/com/proyecto/codedraft/CodedraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.codedraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
